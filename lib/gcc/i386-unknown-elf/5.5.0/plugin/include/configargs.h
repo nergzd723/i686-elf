ed2af0fcbd75e907f1c24584bad141953f89a524
Mark <clashclanacc2602@gmail.com>
2019-11-01 12:39:58 +0200

    init

---
+++ lib/gcc/i386-unknown-elf/5.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/nnext/Desktop/.build/i386-unknown-elf/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=i386-unknown-elf --prefix=/home/nnext/x-tools/i386-unknown-elf --with-local-prefix=/home/nnext/x-tools/i386-unknown-elf/i386-unknown-elf --with-headers=/home/nnext/x-tools/i386-unknown-elf/i386-unknown-elf/include --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG 1.24.0.37-3f461da' --disable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --with-gmp=/home/nnext/Desktop/.build/i386-unknown-elf/buildtools --with-mpfr=/home/nnext/Desktop/.build/i386-unknown-elf/buildtools --with-mpc=/home/nnext/Desktop/.build/i386-unknown-elf/buildtools --with-isl=/home/nnext/Desktop/.build/i386-unknown-elf/buildtools --enable-lto --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-target-optspace --disable-nls --disable-multilib --enable-languages=c";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "i386" }, { "arch", "i386" } };
---
+++ lib/gcc/i686-elf/9.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-9.1.0/configure --target=i686-elf --prefix=/home/nnext/opt/cross --disable-nls --enable-languages=c++,go --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
